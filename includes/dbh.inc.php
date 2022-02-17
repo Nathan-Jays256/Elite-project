8d3adb485b6de58d3e0514c39ec5821b2e9a4f58
Nathan Jays <77129209+Nathan-Jays256@users.noreply.github.com>
2022-02-17 16:48:42 +0300

    make it better

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,10 +1,24 @@
-<!-- <?php
+<?php
+  $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $cleardb_server = $cleardb_url["host"];
+  $cleardb_username = $cleardb_url["user"];
+  $cleardb_password = $cleardb_url["pass"];
+  $cleardb_db = substr($cleardb_url["path"],1);
 
-$conn=mysqli_connect("localhost", "root", "", "elitesdb");
+  $active_group = 'default';
+  $query_builder = True;
+  $conn=mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+
+
+
+
+// $conn=mysqli_connect("localhost", "root", "", "elitesdb");
+
+// if (mysqli_connect_errno())
+//   {
+//   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//   }
+//   $url   = "http://localhost/elite/";	
   
- ?> -->
+//  ?>
